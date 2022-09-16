411d40f266b6e0648311245f9dcd9bc055a0efbc
vsri355 <vsri355@aucklanduni.ac.nz>
2022-09-16 19:40:05 +1200

    added lines from kejia's codes with my own comments

--- remove_multifurcations.py
+++ remove_multifurcations.py
@@ -12,6 +12,18 @@ elements=pg.import_exelem_tree(file_name+'.exelem')
 node = nodes['nodes']
 elem = elements['elems']
 
+#for removing multiple connections in the read in tree these were the lines kejia have used
+seed_geom=seed_geometry_rodent(volume,thickness,ellipticity,cord_insertion_x,cord_insertion_y,umb_artery_length,datapoints_chorion) #this line should be modified to understand the tree
+elem_connect=element_connectivity_1D(seed_geom['nodes'],seed_geom['elems'],7)
+
+#the variables passed in this remove multiple connections function should refer to the read in tree's
+seed_geom,elem_connect=remove_multiple_elements(seed_geom,elem_connect,'non')
+#after this step again the nodes and elems should be updated and assigned with names.
+node=seed_geom['nodes']
+seed_geom['nodes']=node[:,1:]
+
+#something with return() should be here which returns the final tree after removing the multiple connections
+
 output = 1  # set whether output exfiles are made
 if output == 1:
     # cmgui files to be exported
@@ -19,7 +31,5 @@ if output == 1:
     os.chdir(path) # filepath to save exported CMGUIfiles
 pg.export_ex_coords(node, 'vessels', file_name, 'exnode') # exports nodes
 pg.export_exelem_1d(elem, 'vessels', file_name) # exports elems
-
-
 
 
