299235ac6a1b2ffd88e488454818205a60b2df96
vsri355 <vsri355@aucklanduni.ac.nz>
2022-09-16 14:03:05 +1200

    test tree files included

--- remove_multifurcations.py
+++ remove_multifurcations.py
@@ -3,9 +3,9 @@ from placentaAnalysisFunctions import *
 from placentaAnalysis_utilities import *
 from placentagen import imports_and_exports as pg
 
-path = '/hpc/vsri355/Modelling/Updated_rodent_analysis/Mice/BranchList_files-Mice/Con30pt_8' #points to the folder containing the ex nodes and elem files
+path = '/hpc/vsri355/Modelling/Modelling-files/test' #points to the folder containing the ex nodes and elem files
 os.chdir(path)
-file_name = "tree"  # file names of nodes and elems and takes in the node and elem files to be used
+file_name = "trialtree"  # file names of nodes and elems and takes in the node and elem files to be used
 nodes=pg.import_exnode_tree(file_name+'.exnode')
 elements=pg.import_exelem_tree(file_name+'.exelem')
 #initializing nodes and elems
@@ -15,7 +15,7 @@ elem = elements['elems']
 output = 1  # set whether output exfiles are made
 if output == 1:
     # cmgui files to be exported
-    path = '//hpc/vsri355/Modelling/Modelling-files/CMGUI_files/Mice/Con30pt_8'
+    path = '/hpc/vsri355/Modelling/Modelling-files/CMGUI_files/test'
     os.chdir(path) # filepath to save exported CMGUIfiles
 pg.export_ex_coords(node, 'vessels', file_name, 'exnode') # exports nodes
 pg.export_exelem_1d(elem, 'vessels', file_name) # exports elems
