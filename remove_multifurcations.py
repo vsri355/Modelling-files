9f2c6a71165517ec6fa9bb63f239a379eff3d053
vsri355 <vsri355@aucklanduni.ac.nz>
2022-09-20 11:47:42 +1200

    cleaned to remove extra lines

--- remove_multifurcations.py
+++ remove_multifurcations.py
@@ -5,9 +5,6 @@ from placentaAnalysis_utilities_new import *
 path = '/hpc/vsri355/Modelling/Modelling-files/test' #points to the folder containing the ex nodes and elem files
 os.chdir(path)
 file_name = "trialtree"  # file names of nodes and elems and takes in the node and elem files to be used
-#nodes=pg.import_exnode_tree(file_name+'.exnode')
-#elements=pg.import_exelem_tree(file_name+'.exelem')
-
 #for removing multiple connections in the read in tree
 chorion_and_stem = {}  #initializing the tree geometry
 chorion_and_stem['nodes'] = pg.import_exnode_tree(file_name+'.exnode')['nodes'][:, 1:4]
@@ -16,11 +13,6 @@ chorion_and_stem['elems'] = pg.import_exelem_tree(file_name+'.exelem')['elems']
 elem_connectivity = pg.element_connectivity_1D(chorion_and_stem['nodes'],chorion_and_stem['elems'])
 #removing multiple connections
 chorion_and_stem,elem_connectivity=remove_multiple_elements(chorion_and_stem,elem_connectivity,type=None)
-
-
-#something with return() should be here which returns the final tree after removing the multiple connections
-
-
 output = 1  # set whether output exfiles are made
 if output == 1:
     # cmgui files to be exported
